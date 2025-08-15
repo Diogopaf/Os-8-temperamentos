a7f079c461b3be7ba69bb377dc1f631c38ce703a
Diogo <diogopaf91@gmail.com>
2025-08-14 23:57:27 -0300

    Alteração 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* A propriedade 'basename' diz ao router qual é a pasta base do site */}
+    <BrowserRouter basename="/Os-8-temperamentos/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
