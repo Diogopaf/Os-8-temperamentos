42e502c99be2fd92061f28f631978f175c2f8d0e
Diogo <diogopaf91@gmail.com>
2025-08-15 00:06:53 -0300

    Remover o gestor de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,35 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Test from "./pages/Test";
-import Result from "./pages/Result";
-import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+import { Index } from "@/pages/Index";
+import { Test } from "@/pages/Test";
+import { Result } from "@/pages/Result";
+import { NotFound } from "@/pages/NotFound";
+import { Temperaments } from "./pages/Temperaments";
 
+// Este queryClient é necessário para o QueryClientProvider
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        {/* O BrowserRouter foi removido daqui para evitar conflito */}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/test" element={<Test />} />
-          <Route path="/result/:temperamentId" element={<Result />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/result" element={<Result />} />
+          <Route path="/temperaments" element={<Temperaments />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+        
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
